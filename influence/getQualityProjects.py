2e84e75ec8d0c1598501981d4564eba133d064a0
Josh Willis <willis2342@gmail.com>
2014-11-11 11:03:07 -0500

    Fixed some bugs.

--- influence/getQualityProjects.py
+++ influence/getQualityProjects.py
@@ -2,14 +2,14 @@
 import pymongo
 from bson import json_util
 
-client = pymongo.MongoClient('localhost')
-# client = pymongo.MongoClient('da0.eecs.utk.edu')
+# client = pymongo.MongoClient('localhost')
+client = pymongo.MongoClient('da0.eecs.utk.edu')
 
 bitbucket = client['bitbucket']
 forks = bitbucket['forks']
 repoForkCount = bitbucket['repoForkCount']
 
-for fork in forks():
+for fork in forks.find():
 
   forkUrl = fork['url']
   repoName = forkUrl.replace('https://api.bitbucket.org/2.0/repositories/', '').replace('/forks', '')
@@ -19,10 +19,10 @@ for fork in forks():
   if result == None:
     repoDoc = { 'repoName' : repoName, 'count' : forkCount }
     repoForkCount.insert( repoDoc )
-    print 'Inserting ' + repoName + ' with ' + forkCount + ' forks'
+    # print 'Inserting ' + repoName + ' with ' + forkCount + ' forks'
   else:
     repoDoc = result
     repoDoc['count'] += forkCount
     repoForkCount.update( {'_id' : result['_id'] }, { '$set' : repoDoc } )
-    print 'Updating ' + repoName + ' with ' + forkCount + ' forks'
+    # print 'Updating ' + repoName + ' with ' + forkCount + ' forks'
 
