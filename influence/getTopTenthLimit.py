89556371ed4fde10039e1b02f685d4be06310504
Chris Daffron <cdaffron@vols.utk.edu>
2014-11-11 22:53:17 -0500

    Quality projects list and code to get it

---
+++ influence/getTopTenthLimit.py
@@ -0,0 +1,38 @@
+import sys, pickle
+
+forks = []
+
+infile = open('prepR.r', 'r')
+
+count = 0
+first = True
+for line in infile:
+  if first == True:
+    first = False
+    continue
+  splitline = line.split(';')
+  forks.append(int(splitline[2]))
+  if(count % 1000 == 0):
+    print str(count)
+  # if(int(splitline[2]) != 0):
+    # break
+  count += 1
+  # print splitline
+  # sys.exit()
+
+print 'Done collecting data'
+# print forks
+forks.sort()
+forks.reverse()
+length = len(forks)
+
+pickle.dump(forks, open('forks.pickle', 'w'))
+
+top_percent = int(length / 200)
+print 'Threshold is: ', forks[top_percent]
+
+# for i in range(len(forks)):
+  # if(forks[i] == 0):
+    # print str(i)
+    # break
+# print forks
