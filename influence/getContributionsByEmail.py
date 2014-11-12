a7710cbfa2ee92c813711ebfaff13a6bc024e133
Chris Daffron <cdaffron@vols.utk.edu>
2014-11-11 23:11:08 -0500

    contribution code

---
+++ influence/getContributionsByEmail.py
@@ -0,0 +1,30 @@
+import sys, pickle, json
+
+contributions = {}
+
+infile = open('/home/audris/measures.out', 'r')
+
+count = 0
+for line in infile:
+  splitline = line.split(';')
+  authors = splitline[4].split(':')
+  repo = splitline[0]
+  for author in authors:
+    if author in contributions:
+      contributions[author].append(repo)
+    else:
+      contributions[author] = [repo]
+  # forks.append(int(splitline[2]))
+  if(count == 10):
+    break
+  if(count % 1000 == 0):
+    print str(count)
+  # if(int(splitline[2]) != 0):
+    # break
+  count += 1
+  # print splitline
+  # sys.exit()
+
+print 'Done collecting data'
+outfile = open('userContrib.json', 'w')
+json.dump(contributions, outfile)
